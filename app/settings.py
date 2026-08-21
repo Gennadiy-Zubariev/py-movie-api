aa1f3e616247d87d2a58aae47714aacf56ec9cbd
Gena <genada89@gmail.com>
2026-08-21 23:45:54 +0300

    fix: add serializer.is_valid(raise_exception=True) instead if, fix admin site registration

--- app/settings.py
+++ app/settings.py
@@ -122,10 +122,6 @@ STATIC_URL = "static/"
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
-MAILERS = {
-    "default": {
-        "BACKEND": "django.core.mail.backends.console.EmailBackend",
-    },
-}
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
